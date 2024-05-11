40bb30e6bfe763e1f21a2e570365cb68e41c2c22
Douglas Storz <douglasstorz@gmail.com>
2024-05-11 02:52:31 -0400

    Basic scaffolding for tic tac toe

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Level1Component } from './level1/level1.component';
+import { Level2Component } from './level2/level2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'level1', component: Level1Component },
+  { path: 'level2', component: Level2Component },
+
+  { path: '', pathMatch: 'full', redirectTo: 'level1' },
+];
