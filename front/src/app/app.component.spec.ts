9013fa550236419ba6eba62625716e7d05968b7f
Douglas Storz <douglasstorz@gmail.com>
2024-05-12 05:27:10 -0400

    Set up test runner for firefox and all prefab tests pass

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, RouterModule.forRoot(routes)],
     }).compileComponents();
   });
 
@@ -24,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Games');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Tic Tac Toe');
   });
 });
