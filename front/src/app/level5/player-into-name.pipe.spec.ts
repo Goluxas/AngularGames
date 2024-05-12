6b78a999f559662b9b5bb1681f72b56d61ea5eed
Douglas Storz <douglasstorz@gmail.com>
2024-05-12 05:52:27 -0400

    Unit tests for pipes

--- front/src/app/level5/player-into-name.pipe.spec.ts
+++ front/src/app/level5/player-into-name.pipe.spec.ts
@@ -1,8 +1,23 @@
 import { PlayerIntoNamePipe } from './player-into-name.pipe';
 
 describe('PlayerIntoNamePipe', () => {
+  const pipe = new PlayerIntoNamePipe();
+
   it('create an instance', () => {
-    const pipe = new PlayerIntoNamePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('transforms 1 into X', () => {
+    expect(pipe.transform(1)).toBe('X');
+  });
+
+  it('transforms 2 into O', () => {
+    expect(pipe.transform(2)).toBe('O');
+  });
+
+  describe('Error cases', () => {
+    it('throws on invalid player ID', () => {
+      expect(() => pipe.transform(3)).toThrow();
+    });
   });
 });
--- front/src/app/level5/player-name-into-class.pipe.spec.ts
+++ front/src/app/level5/player-name-into-class.pipe.spec.ts
@@ -1,8 +1,16 @@
 import { PlayerNameIntoClassPipe } from './player-name-into-class.pipe';
 
 describe('PlayerNameIntoClassPipe', () => {
+  const pipe = new PlayerNameIntoClassPipe();
+
   it('create an instance', () => {
-    const pipe = new PlayerNameIntoClassPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('transforms input to occupied-{input}, lowercase', () => {
+    expect(pipe.transform('X')).toBe('occupied-x');
+    expect(pipe.transform('O')).toBe('occupied-o');
+    expect(pipe.transform('')).toBe('occupied-');
+    expect(pipe.transform('ZeBrA')).toBe('occupied-zebra');
   });
 });
