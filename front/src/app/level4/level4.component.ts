da0ea270693562bdc2808b858cf5ce7a2cd0019b
Douglas Storz <douglasstorz@gmail.com>
2024-05-12 04:05:14 -0400

    Split business and view logic for Level4

---
+++ front/src/app/level4/level4.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { Cell, Level3Component } from '../level3/level3.component';
+import { BoardService } from './board.service';
+
+@Component({
+  selector: 'app-level4',
+  standalone: true,
+  imports: [],
+  templateUrl: './level4.component.html',
+  styleUrl: './level4.component.scss',
+})
+export class Level4Component implements OnInit {
+  public playerNames: string[] = ['', 'X', 'O'];
+  private boardContent: number[][] = [];
+
+  constructor(private board: BoardService) {}
+
+  ngOnInit(): void {
+    this.boardContent = this.board.boardContent;
+  }
+
+  public getPlayerName(col: number, row: number): string {
+    return this.playerNames[this.boardContent[row][col]];
+  }
+
+  public getStyle(col: number, row: number): string {
+    if (this.getPlayerName(col, row) === '') return '';
+
+    return `occupied-${this.getPlayerName(col, row).toLowerCase()}`;
+  }
+
+  public getCells(): Cell[][] {
+    let cells: Cell[][] = [];
+
+    for (let row = 0; row < 3; row++) {
+      cells.push([]);
+      for (let col = 0; col < 3; col++) {
+        let cell: Cell = {
+          class: this.getStyle(col, row),
+          playerName: this.getPlayerName(col, row),
+        };
+
+        cells[row][col] = cell;
+      }
+    }
+
+    return cells;
+  }
+
+  restart(): void {
+    this.board.restart();
+  }
+
+  currentWinner(): number {
+    return this.board.currentWinner;
+  }
+
+  set(col: number, row: number) {
+    this.board.set(col, row);
+  }
+}
